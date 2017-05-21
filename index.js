083dbe52a308920514dd13115373f9dfa1ddd3bd
Roberto Mauro <erremauro@icloud.com>
2017-05-21 22:21:31 +0200

    Fix jsx-a11y img-has-alt rule requirement

--- index.js
+++ index.js
@@ -204,10 +204,8 @@ module.exports = {
 
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
     'jsx-a11y/aria-role': 'warn',
-    'jsx-a11y/img-has-alt': 'warn',
     'jsx-a11y/img-redundant-alt': 'warn',
     'jsx-a11y/no-access-key': 'warn',
-    'jsx-a11y/img-has-alt': 'warn',
 
     // https://github.com/gajus/eslint-plugin-flowtype
     'flowtype/define-flow-type': 'warn',
